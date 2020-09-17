568c5ef79ff4248ef48409c138707d1a3221b2f7
jimmy <jimmy@luno.com>
2020-09-17 08:52:21 +0200

    devopsy stuff

--- build.sh
+++ build.sh
@@ -2,28 +2,36 @@
 
 set -x
 
+# Service to build
+SERVICE=${1}
+# TODO(): Check errors    
+TAG=$(($(docker images jimbones/pitchin-${SERVICE} -q --format "{{.Tag}}" | sort -n | tail -1) + 1))
+
+if [ -z "${SERVICE}" ]
+then
+    echo "service is empty"
+    exit 1
+fi
+
+if [ ! -d "./${SERVICE}" ]
+then
+    echo "service ${SERVICE} does not exist"
+    exit 1
+fi
+
 # Checkout latest
 git checkout master
 git pull
 
 # Build backend
 (
-    cd backend
+    cd ${SERVICE}
     npm install
     npm run build
+
+    docker build -t jimbones/pitchin-${SERVICE}:latest -t jimbones/pitchin-${SERVICE}:3 .
+    docker push     jimbones/pitchin-${SERVICE}
 )
 
-# Build frontend
-(
-    cd frontend
-    npm install
-    npm run build
-)
-
-# Build backend
-docker-compose build back-end
-docker-compose up -d back-end
-
-# Build frontend
-docker-compose build front-end
-docker-compose up -d front-end
+# Deploy service
+ansible-playbook  playbook.yml --extra-vars "tag=${TAG}" --extra-vars "service=${SERVICE}"
